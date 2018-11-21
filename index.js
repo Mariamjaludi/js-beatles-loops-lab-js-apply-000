ca951d22b2185bcc0703fbc35b4c128d893bafec
Mariam Jaludi <mariam.jaludi@gmail.com>
2018-11-21 17:57:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 // add solution here
 function iLoveTheBeatles(number){
   var temp = [];
+  var i = 0;
+  do {
+    temp[i] = "I love the Beatles!";
+    number++;
+    i++;
+  } while (number < 15)
+  return temp;
 }
